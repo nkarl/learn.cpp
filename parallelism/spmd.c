fe5d580b71ac87b1f3c6559a6a8e7915359a842b
nkarl <21993921+nkarl@users.noreply.github.com>
2021-09-26 22:16:36 -0700

    added comment to spmd.c

--- parallelism/spmd.c
+++ parallelism/spmd.c
@@ -2,35 +2,39 @@
 #include <stdio.h>
 
 static long NUM_STEPS = 100000;
+double step;
 
 #define NUM_THREADS 2
 
 void main() {
-    int i, nTHREADs;
-    double pi, sum[NUM_THREADS];
-    double step = 1.0/(double) NUM_STEPS;
+  int i, nTHREADs;
+  double pi, sum[NUM_THREADS];
 
-    omp_set_num_threads(NUM_THREADS);
+  step = 1.0 / (double)NUM_STEPS;
+  printf("step=%f\n", step);
 
-# pragma omp parallel
-    {
-        int i, id, nTHRDs;
-        double x;
-        
-        id = omp_get_thread_num();
+  omp_set_num_threads(NUM_THREADS);     // set the number of threads =2
 
-        nTHRDs = omp_get_num_threads();
+  /*Create a new team of worker threads */
+#pragma omp parallel
+  {
+    int i, id, nTHRDs;
+    double x;
 
-        if (id == 0) nTHREADs = nTHRDs;
-        for (i=id, sum[id]=0.0; i<NUM_STEPS; i=i+nTHRDs) {
-            x = (i+0.5)*step;
+    id = omp_get_thread_num();          // assign the thread# to id
+    nTHRDs = omp_get_num_threads();     // assign the num_threads to the local var
 
-            sum[id] += 4.0/(1.0+x*x);
-        }
+    if (id == 0)
+      nTHREADs = nTHRDs;
+
+    for (i = id, sum[id] = 0.0; i < NUM_STEPS; i = i + nTHRDs) {
+      x = (i + 0.5) * step;
+      sum[id] += 4.0 / (1.0 + x * x);
     }
+  }
 
-    for (i=0, pi=0.0; i<nTHREADs; i++)
-        pi += sum[i] * step;
+  for (i = 0, pi = 0.0; i < nTHREADs; i++)
+    pi += sum[i] * step;
 
-    printf("%f\n", pi);
+  printf("\nresult:\npi=%f\n", pi);
 }
