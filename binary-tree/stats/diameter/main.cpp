d0061d8a1f01706725c368ddcc86a2845612352e
nkarl <21993921+nkarl@users.noreply.github.com>
2024-01-20 12:02:30 -0800

    Updated.

--- binary-tree/stats/diameter/main.cpp
+++ binary-tree/stats/diameter/main.cpp
@@ -12,11 +12,29 @@ struct TreeStats {
   int diameter, longest;
 };
 
+/**
+ * NOTE: in order to understand how this works, useful to have these two things:
+ *  1. an image of the stacktrace.
+ *  2. induction proof.
+ *
+ * NOTE: INDUCTION PROOF
+ *  - hypothesis: every tree node has a diameter and a longer branch between its left and right childs.
+ *  - proof:
+ *    - leaf nodes are trivial, { d: 0, l: 0 }
+ *    - for every tree node,
+ *      - Its diameter is the sum of its left child's longest and right child's longest.
+ *      - Its longest branch is either left or right.
+ *      - If its diameter is more than the diameter of its left or right child, then including itself,
+ *        - its longest has to be larger than the depth of its either childs.
+ *        - in other words, its diameter and longest are maximal so far.
+ *      - Once the stacktrace pops back at the root node, the diameter and longest are maximal.
+ */
 TreeStats find_tree_diameter(Node *node) {
   if (!node)
     return {0, 0};
-  auto left = find_tree_diameter(node->left);
-  auto right = find_tree_diameter(node->right);
+
+  auto left   = find_tree_diameter(node->left);
+  auto right  = find_tree_diameter(node->right);
 
   auto node_d = left.longest + right.longest;
   return {std::max(node_d, std::max(left.diameter, right.diameter)),
